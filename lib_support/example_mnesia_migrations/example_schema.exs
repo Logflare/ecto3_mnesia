d667e2dfcd928cb53366adc592eedd84a86985f4
Pascal Knoth <io.pascal.knoth@gmail.com>
2020-12-11 10:55:08 +0100

    do not compile example files

--- lib_support/example_mnesia_migrations/example_schema.exs
+++ lib_support/example_mnesia_migrations/example_schema.exs
@@ -1,10 +1,12 @@
 defmodule BinaryIdTestSchema do
+  @moduledoc false
+
   use Ecto.Schema
-  
+
   @primary_key {:id, :binary_id, autogenerate: true}
   schema "binary_id_table" do
     timestamps()
-    
+
     field(:field, :string)
   end
 end
